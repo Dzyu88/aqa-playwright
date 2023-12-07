029e29c81ae6a7be0500cfb8a6020598fede7a65
Dzyu88 <you@example.comdzyu88@gmail.com>
2023-12-08 01:06:27 +0200

    Fixed mistakes in profileAPI.spec.mjs

--- tests/e2e/profile/profileAPI.spec.mjs
+++ tests/e2e/profile/profileAPI.spec.mjs
@@ -3,7 +3,7 @@ import {CUSTOM_PROFILES_DATA_RESPONSE_BODY} from "./fixtures/profilesData.js";
 import {expect} from "@playwright/test";
 
 test.describe.skip("Interception of response body on Profile page", ()=>{
-    test.skip('Frontend should send correct request to get data', async ({userProfilePage})=>{
+    test.only('Frontend should send correct request to get data', async ({userProfilePage})=>{
         const { page } = userProfilePage
 
         await page.route('/api/users/profile', route => route.abort())
@@ -20,7 +20,7 @@ test.describe.skip("Interception of response body on Profile page", ()=>{
         })
 
         await page.reload()
-        await expect(page.getByText(`Oleksandr Dziubas`)).toBeVisible()
+        await expect(page.getByText(this.userName)).toBeVisible()
 
     })
 })
